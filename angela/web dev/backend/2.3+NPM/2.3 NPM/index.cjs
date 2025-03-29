82e4755a1775917906c2a226158e21848df807c7
himaleswarreddy <himaleswarreddy@gmail.com>
2025-03-29 12:50:11 +0530

    node js basics

---
+++ angela/web dev/backend/2.3+NPM/2.3 NPM/index.cjs
@@ -0,0 +1,13 @@
+// var generateName = require("sillyname");
+
+// import generateName from "sillyname";
+// var sillyName = generateName();
+
+// console.log(`My name is ${sillyName}.`);
+
+const superheroes = require("superheroes");
+
+const name = superheroes.random();
+console.log(`Your superhero name is: ${name}`);
+
+
